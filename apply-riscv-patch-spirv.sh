3e90c8469255e782e12d73ad0d37a4cca54055fa
Juan Fumero <jjfumero@gmail.com>
2025-04-15 12:43:05 +0200

    Patch for RISC-V port of TornadoVM

---
+++ apply-riscv-patch-spirv.sh
@@ -0,0 +1,16 @@
+VERSION=1.1.1-dev
+pushd .
+cd tornado-drivers/opencl-jni/src/main/cpp/
+mkdir -p build/
+cd build/
+rm -Rf *
+cmake ..
+make
+popd
+patch -p 1 < tornadovm-riscv-patch/patch.diff
+./bin/tornadovm-installer --jdk liberica-jdk-21 --backend=spirv
+source setvars.sh
+mkdir -p bin/sdk/lib
+cp tornado-drivers/opencl-jni/src/main/cpp/build/libtornado-opencl.so bin/sdk/lib/
+cp tornado-drivers/opencl-jni/target/tornado-drivers-opencl-jni-$VERSION*.jar bin/sdk/share/java/tornado/tornado-drivers-opencl-jni-$VERSION.jar 
+tornado --devices
